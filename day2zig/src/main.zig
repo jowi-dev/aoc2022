0357625962c510d5dd507b7d9b3431e5c7fd4d1f
Joe Williams <jowi@pop-os.localdomain>
2022-12-09 10:03:47 -0500

    readability because that matters

--- day2zig/src/main.zig
+++ day2zig/src/main.zig
@@ -1,37 +1,18 @@
 const std = @import("std");
 
-/// Opponents Choice
-/// A - Rock
-/// B - Paper
-/// C - Scissors
 
-const opponentChoice = enum {
-    A,
-    B,
-    C
+const choice = enum {
+    ROCK,
+    PAPER, 
+    SCISSORS
 };
 
-/// My Choice & Points
-/// X - Rock - 1
-/// Y - Paper - 2
-/// Z - Scissors -3 
-const playerChoice = enum {
-    X,
-    Y,
-    Z
-};
-/// Outcome Points
-/// 0 - Loss 
-/// 3 - Draw
-/// 6 - Win
 
 const ChoiceNotFoundError = error {
     ChoiceNotFound
 };
 
-//pub fn main(filename :*const [10:0]u8) !u64 {
-pub fn main() !void{
-    const filename = "./real.txt";
+pub fn main(filename :*const [10:0]u8) !u64 {
     var total :u64= 0;
     var file = try std.fs.cwd().openFile(filename, .{});
     defer file.close();
@@ -46,53 +27,65 @@ pub fn main() !void{
         total += getGameScore(opponentInput, playerInput) + getPlayerChoiceScore(playerInput);
     }
     std.debug.print("Total Is: {}\n", .{total});
+    return total;
 }
 
-fn parseOpponent(opponentInput : u8) !opponentChoice {
+/// Opponents Choice
+/// A - Rock
+/// B - Paper
+/// C - Scissors
+fn parseOpponent(opponentInput : u8) !choice {
     return switch(opponentInput) {
-        'A' => .A, 
-        'B' => .B, 
-        'C' => .C,
+        'A' => .ROCK, 
+        'B' => .PAPER, 
+        'C' => .SCISSORS,
         else => error.ChoiceNotFound 
     };
 }
 
-fn parsePlayer(playerInput : u8) !playerChoice {
+/// My Choice & Points
+/// X - Rock 
+/// Y - Paper 
+/// Z - Scissors 
+fn parsePlayer(playerInput : u8) !choice {
     return switch(playerInput) {
-        'X' => .X, 
-        'Y' => .Y, 
-        'Z' => .Z,
+        'X' => .ROCK, 
+        'Y' => .PAPER, 
+        'Z' => .SCISSORS,
         else => error.ChoiceNotFound 
     };
 }
 
 
 
-// Be sure to add a type after the parenthesis and before the {
-pub fn getGameScore(opponentInput : opponentChoice, playerInput : playerChoice)  u8 {
+/// Outcome Points
+/// 0 - Loss 
+/// 3 - Draw
+/// 6 - Win
+pub fn getGameScore(opponentInput : choice, playerInput : choice)  u8 {
     var score :u8= 0;
     score = switch (opponentInput) {
-        .A => rock : {
+        .ROCK => rock : {
             score = switch (playerInput) {
-                .X => 3,
-                .Y => 6,
-                .Z => 0,
+                .ROCK => 3,
+                .PAPER => 6,
+                .SCISSORS => 0,
             };
             break :rock score;
         },
-        .B => paper: {
+        .PAPER => paper: {
             score =  switch (playerInput) {
-                playerChoice.X => 0,
-                playerChoice.Y => 3,
-                playerChoice.Z => 6,
+                .ROCK => 0,
+                .PAPER => 3,
+                .SCISSORS => 6,
             };
             break :paper score;
         },
-        .C => scissors: {
+        .SCISSORS => scissors: {
             score =  switch (playerInput) {
-                playerChoice.X => 6,
-                playerChoice.Y => 0,
-                playerChoice.Z => 3,
+                .ROCK => 6,
+                .PAPER => 0,
+                .SCISSORS => 3,
             };
             break :scissors score;
         }
@@ -101,49 +94,58 @@ pub fn getGameScore(opponentInput : opponentChoice, playerInput : playerChoice) 
 }
 
 
-// Be sure to add a type after the parenthesis and before the {
-pub fn getPlayerChoiceScore(playerInput: playerChoice) u8{
+/// My Choice & Points
+/// Rock - 1
+/// Paper - 2
+/// Scissors -3 
+pub fn getPlayerChoiceScore(playerInput: choice) u8{
     return switch (playerInput) {
-        .X => 1,
-        .Y => 2,
-        .Z => 3
+        .ROCK => 1,
+        .PAPER => 2,
+        .SCISSORS => 3
     } ;
 }
 
 
+test "real strategy guide works" {
+    const result = try main("./real.txt");
+    std.debug.print("{}", .{result});
+
+    try std.testing.expect(result > 0);
+}
 test "simple strategy guide works" {
     try std.testing.expectEqual(main("./test.txt"), 15);
 }
 
 
 test "When the opponent chooses rock, game handles player cases correctly" {
-    try std.testing.expectEqual(getGameScore(opponentChoice.A, playerChoice.X), 3);
-    try std.testing.expectEqual(getGameScore(opponentChoice.A, playerChoice.Y), 6);
-    try std.testing.expectEqual(getGameScore(opponentChoice.A, playerChoice.Z), 0);
+    try std.testing.expectEqual(getGameScore(choice.ROCK, choice.ROCK), 3);
+    try std.testing.expectEqual(getGameScore(choice.ROCK, choice.PAPER), 6);
+    try std.testing.expectEqual(getGameScore(choice.ROCK, choice.SCISSORS), 0);
 }
 
 test "When the opponent chooses paper, game handles player cases correctly" {
-    try std.testing.expectEqual(getGameScore(opponentChoice.B, playerChoice.X), 0);
-    try std.testing.expectEqual(getGameScore(opponentChoice.B, playerChoice.Y), 3);
-    try std.testing.expectEqual(getGameScore(opponentChoice.B, playerChoice.Z), 6);
+    try std.testing.expectEqual(getGameScore(choice.PAPER, choice.ROCK), 0);
+    try std.testing.expectEqual(getGameScore(choice.PAPER, choice.PAPER), 3);
+    try std.testing.expectEqual(getGameScore(choice.PAPER, choice.SCISSORS), 6);
 }
 
 test "When the opponent chooses scissors, game handles player cases correctly" {
-    try std.testing.expectEqual(getGameScore(opponentChoice.C, playerChoice.X), 6);
-    try std.testing.expectEqual(getGameScore(opponentChoice.C, playerChoice.Y), 0);
-    try std.testing.expectEqual(getGameScore(opponentChoice.C, playerChoice.Z), 3);
+    try std.testing.expectEqual(getGameScore(choice.SCISSORS, choice.ROCK), 6);
+    try std.testing.expectEqual(getGameScore(choice.SCISSORS, choice.PAPER), 0);
+    try std.testing.expectEqual(getGameScore(choice.SCISSORS, choice.SCISSORS), 3);
 }
 
 test "When a player chooses Rock, 1 point" {
-    try std.testing.expectEqual(getPlayerChoiceScore(playerChoice.X), 1);
+    try std.testing.expectEqual(getPlayerChoiceScore(choice.ROCK), 1);
 
 }
 test "When a player chooses Paper, 2 point" {
-    try std.testing.expectEqual(getPlayerChoiceScore(playerChoice.Y), 2);
+    try std.testing.expectEqual(getPlayerChoiceScore(choice.PAPER), 2);
 
 }
 test "When a player chooses Scissors, 3 point" {
-    try std.testing.expectEqual(getPlayerChoiceScore(playerChoice.Z), 3);
+    try std.testing.expectEqual(getPlayerChoiceScore(choice.SCISSORS), 3);
 
 }
 
